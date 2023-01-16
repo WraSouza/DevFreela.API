85fb8409cbb2a6600dde1478d5f2f6a131162f8d
Wladimir Ribeiro de Souza <wladimirsouza@outlook.com>
2023-01-16 16:49:36 -0300

    Criando a camada de aplicação.

--- DevFreela.Application/ViewModels/ProjectDetailsViewModel.cs
+++ DevFreela.Application/ViewModels/ProjectDetailsViewModel.cs
@@ -8,5 +8,22 @@ namespace DevFreela.Application.ViewModels
 {
     public class ProjectDetailsViewModel
     {
+        public ProjectDetailsViewModel(int id, string title, string description, decimal totalCost, DateTime? startedAt, DateTime? startedFinished)
+        {
+            Id = id;
+            Title = title;
+            Description = description;
+            TotalCost = totalCost;
+            StartedAt = startedAt;
+            StartedFinished = startedFinished;
+        }
+
+        public int Id { get; private set; }
+        public string Title { get; private set; }
+        public string Description { get; private set; }
+        public decimal TotalCost { get; private set; }
+        public DateTime? StartedAt { get; private set; }
+        public DateTime? StartedFinished { get; private set; }
+
     }
 }
